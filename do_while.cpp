995d1a4d82084e05a55f8409901e838efd920073
YRlearner <35609645+YRlearner@users.noreply.github.com>
2020-11-05 15:43:35 +0100

    Create do_while.cpp

---
+++ do_while.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std ;
+
+int main()
+{
+   int x=0;
+            do {
+                cout<<"hello fuckers,opps\n";
+                x++;
+                cout<<x<<endl;
+            }
+            while(x<=10);
+            return 0;
+}
