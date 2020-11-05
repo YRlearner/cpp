2af46fea7cab675b40ce4ce1da908903c13c8d72
YRlearner <35609645+YRlearner@users.noreply.github.com>
2020-11-05 15:39:31 +0100

    Create while.cpp

---
+++ while.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+
+using namespace std ;
+
+int main()
+{
+   int x=0;
+   while (x<=100)
+    {
+    cout<<x <<" x fuck"<<endl;
+    x++;
+    }
+            return 0;
+}
